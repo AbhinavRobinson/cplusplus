c4404ca76a2472cbf874f55ac2f31a6be2a5a2d8
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-19 15:45:51 +0530

    (feat): add sqrcbe program

--- programs/somebasicmath/squarecube.cpp
+++ programs/somebasicmath/squarecube.cpp
@@ -7,6 +7,17 @@
 */
 int main()
 {
+  short int num;
+
+  printf("Enter a number for square and cube: ");
+  std::cin >> num;
+
+  int sqr = num * num;
+  int cbe = sqr * num;
+
+  std::cout << "Square: " << sqr << std::endl;
+  std::cout << "Cube: " << cbe << std::endl;
+
   //clear buffer, wait for input to close program
   std::cout << "Enter Anything to Quit :)" << std::endl;
   std::cin.clear();
