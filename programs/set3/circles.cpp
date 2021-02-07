8dc8c09386ae88ed448eadf4d0ed236fd86713bd
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-07 16:47:05 +0530

    (fix): cmp circles

--- programs/set3/circles.cpp
+++ programs/set3/circles.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 #include <limits.h>
 #include <numeric>
+#include <math.h>
+
+/** Returns the area of the circle
+ * @param radius {unsigned int}
+ * @returns area {float}
+ */
+float calcArea(unsigned radius)
+{
+  return floor(3.1415 * pow(radius, 2));
+}
+
+/** Returns the radius of the circle
+ * @param area {float}
+ * @returns radius {unsigned int}
+*/
+int calcRadius(float area)
+{
+  return pow(area / 3.1415, 0.5);
+}
 
 /** Prints area of circles.
  * @author Abhinav Robinson
@@ -19,8 +38,22 @@ int main()
   std::cin >> r2;
 
   // Now we have the Radius, lets find the area.
+  float A1 = calcArea(r1);
+  float A2 = calcArea(r2);
 
-  std::cout << r1 << " " << r2 << std::endl;
+  // now we will print the area.
+  std::cout << "Areas are " << A1 << " and  " << A2 << std::endl;
+
+  // lets merge and circles.
+  float A3 = A1 + A2;
+
+  // Find new radius
+  int r3 = calcRadius(A3);
+
+  // Print the third radius.
+  std::cout << "The radius of the third area is " << r3 << std::endl;
+
+  // lets find the radius of the mergerd circle.
 
   return 0;
 }
