9ca6735993aeb2b2497b04d44578e1b45cdd0487
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-07 16:30:17 +0530

    (fix): add circles

--- programs/set3/circles.cpp
+++ programs/set3/circles.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <limits.h>
-
 #include <numeric>
 
 int main()
 {
+  std::cout << "Finds area of circles" << std::endl;
+  int r1, r2;
+
+  std::cout << "Enter first radius ? ";
+  std::cin >> r1;
+
+  std::cout << "Enter second radius ? ";
+  std::cin >> r2;
+
+  std::cout << r1 << " " << r2 << std::endl;
   return 0;
 }
