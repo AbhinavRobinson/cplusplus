ce27deba8f18f9c02c6e88d7a34f89590f9ba1e5
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-09 14:26:10 +0530

    (feat): add not found condition

--- programs/set3/paircompare.cpp
+++ programs/set3/paircompare.cpp
@@ -5,6 +5,22 @@
 
 using namespace std;
 
+/**
+ * Compare pairs (w,x), (y,z) on given conditions :
+ * 
+ *  W < Y , X > Z  =>  (total pairs) : (list of pair of pairs) => 1 : ((1,4),(3,2))              
+ *  and                                                                                                     
+ *  W > Y , X < Z  =>  (total pairs) : (list of pair of pairs) => 1 : ((13,12),(11,14))                     
+ *                                                                                                          
+ *  if no such pair  =>  Not Found                                                                          
+ *                                                                                                          
+ *  given (in line 1) Array of pairs.                                                                       
+ * 
+ */
+
+bool l1HasNoPairs = true;
+bool l2HasNoPairs = true;
+
 /** Case 1:  W < Y , X > Z
  * @param arr list of pairs
  * @returns list of pairs
@@ -42,6 +58,7 @@ auto getCase1List(vector<vector<int>> arr)
     {
       // since we did find pairs, we will push local pair list to array
       array.push_back(pairs);
+      l1HasNoPairs = false;
     }
 
     // clear memory
@@ -89,6 +106,7 @@ auto getCase2List(vector<vector<int>> arr)
     {
       // since we did find pairs, we will push local pair list to array
       array.push_back(pairs);
+      l2HasNoPairs = false;
     }
 
     // clear memory
@@ -99,16 +117,7 @@ auto getCase2List(vector<vector<int>> arr)
   return array;
 }
 
-/** Compare pairs (w,x), (y,z) on given conditions :
- * 
- *  W < Y , X > Z  =>  (total pairs) : (list of pair of pairs) => 1 : ((1,4),(3,2))              
- *  and                                                                                                     
- *  W > Y , X < Z  =>  (total pairs) : (list of pair of pairs) => 1 : ((13,12),(11,14))                     
- *                                                                                                          
- *  if no such pair  =>  Not Found                                                                          
- *                                                                                                          
- *  given (in line 1) Array of pairs.                                                                       
- * 
+/** 
  * @author Abhinav Robinson
  * @return {unsigned int} 0
 */
@@ -117,44 +126,61 @@ int main()
   // Create example array
   vector<vector<int>> arr = {{1, 4}, {3, 2}, {5, 6}, {7, 8}, {9, 10}, {13, 12}, {11, 14}};
 
+  bool l1HasPairs;
+  bool l2HasPairs;
+
   // lets generate a list for case 1
   vector<vector<vector<int>>> l1 = getCase1List(arr);
   // lets generate a list for case 2
   vector<vector<vector<int>>> l2 = getCase2List(arr);
 
   // Print contents of case 1
-  cout << "Case 1 size=" << l1.size() << " : ";
-  for (vector<vector<vector<int>>>::const_iterator i = l1.begin(); i != l1.end(); ++i)
+  if (!l1HasNoPairs)
   {
-    for (vector<vector<int>>::const_iterator j = i->begin(); j != i->end(); ++j)
+    cout << "Case 1 size=" << l1.size() << " : ";
+    for (vector<vector<vector<int>>>::const_iterator i = l1.begin(); i != l1.end(); ++i)
     {
-      cout << "(";
-      for (vector<int>::const_iterator k = j->begin(); k != j->end(); ++k)
+      for (vector<vector<int>>::const_iterator j = i->begin(); j != i->end(); ++j)
       {
-        cout << *k << ',';
+        cout << "(";
+        for (vector<int>::const_iterator k = j->begin(); k != j->end(); ++k)
+        {
+          cout << *k << ',';
+        }
+        cout << ")"
+             << " ";
       }
-      cout << ")"
-           << " ";
     }
+    cout << endl;
   }
-  cout << endl;
+  else
+  {
+    cout << "Case 1 Not found" << endl;
+  }
 
   // Print contents of case 2
-  cout << "Case 2 size=" << l2.size() << " : ";
-  for (vector<vector<vector<int>>>::const_iterator i = l2.begin(); i != l2.end(); ++i)
+  if (!l2HasNoPairs)
   {
-    for (vector<vector<int>>::const_iterator j = i->begin(); j != i->end(); ++j)
+    cout << "Case 2 size=" << l2.size() << " : ";
+    for (vector<vector<vector<int>>>::const_iterator i = l2.begin(); i != l2.end(); ++i)
     {
-      cout << "(";
-      for (vector<int>::const_iterator k = j->begin(); k != j->end(); ++k)
+      for (vector<vector<int>>::const_iterator j = i->begin(); j != i->end(); ++j)
       {
-        cout << *k << ',';
+        cout << "(";
+        for (vector<int>::const_iterator k = j->begin(); k != j->end(); ++k)
+        {
+          cout << *k << ',';
+        }
+        cout << ")"
+             << " ";
       }
-      cout << ")"
-           << " ";
     }
+    cout << endl;
   }
-  cout << endl;
+  else
+  {
+    cout << "Case 2 Not found" << endl;
+  }
 
   // important: clean up memory
   printf("\n");
