c8cf990763c58137e191197cdeb75c0430a48ff2
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-09 15:08:15 +0530

    (feat): Add powerset gen

--- programs/set3/clustergen.cpp
+++ programs/set3/clustergen.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 #include <limits.h>
 #include <math.h>
+#include <vector>
+
+using namespace std;
+
+void genPowerset(int n)
+{
+  vector<int> stack;
+  int k;
+  stack.push_back(0); /* 0 is not considered as part of the set */
+  k = 0;
+  vector<vector<int>> powerset;
+  while (1)
+  {
+    if (stack[k] < n)
+    {
+      stack.push_back(stack[k] + 1);
+      k++;
+    }
+    else
+    {
+      stack[k - 1]++;
+      k--;
+    }
+    if (k == 0)
+      break;
+    powerset.push_back(stack);
+  }
+  return;
+}
 
 /**
  * @author Abhinav Robinson
@@ -8,5 +37,10 @@
 */
 int main()
 {
+  int n;
+
+  cout << "Enter the number of records :";
+  cin >> n;
+
   return 0;
 }
