60b8b4871898474084a55d3fbca764bdff95ffac
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-09 15:26:44 +0530

    (feat): make powerset and print

--- programs/set3/clustergen.cpp
+++ programs/set3/clustergen.cpp
@@ -2,16 +2,17 @@
 #include <limits.h>
 #include <math.h>
 #include <vector>
+#include <set>
 
 using namespace std;
 
-void genPowerset(int n)
+auto genPowerset(int n)
 {
   vector<int> stack;
   int k;
   stack.push_back(0); /* 0 is not considered as part of the set */
   k = 0;
-  vector<vector<int>> powerset;
+  vector<vector<int>> powerVec;
   while (1)
   {
     if (stack[k] < n)
@@ -21,14 +22,48 @@ void genPowerset(int n)
     }
     else
     {
-      stack[k - 1]++;
+      stack[k - 1] = stack[k - 1] + 1;
       k--;
     }
     if (k == 0)
       break;
-    powerset.push_back(stack);
+    powerVec.push_back(stack);
   }
-  return;
+
+  vector<set<int>> powerset;
+
+  for (vector<vector<int>>::const_iterator i = powerVec.begin(); i != powerVec.end(); ++i)
+  {
+    set<int> s;
+
+    for (vector<int>::const_iterator j = i->begin(); j != i->end(); ++j)
+    {
+      if (*j == 0)
+      {
+        continue;
+      }
+      s.insert(*j);
+    }
+
+    powerset.push_back(s);
+  }
+
+  return powerset;
+}
+
+void print2DVectorWithCurlies(vector<set<int>> vec)
+{
+  for (vector<set<int>>::const_iterator i = vec.begin(); i != vec.end(); ++i)
+  {
+    cout << "{";
+    for (set<int>::const_iterator j = i->begin(); j != i->end(); ++j)
+    {
+      cout << *j << ',';
+    }
+    cout << "}"
+         << " ";
+  }
+  cout << endl;
 }
 
 /**
@@ -41,6 +76,8 @@ int main()
 
   cout << "Enter the number of records :";
   cin >> n;
+
+  print2DVectorWithCurlies(genPowerset(n));
 
   return 0;
 }
