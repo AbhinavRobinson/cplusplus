7aa9b1560c06d2c3ed92ba6bb046f1e0c63287d4
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-10 17:08:05 +0530

    (feat): gen pairsum

--- programs/set4/maxpairXor.cpp
+++ programs/set4/maxpairXor.cpp
@@ -4,8 +4,29 @@
 #include <math.h>
 #include <limits.h>
 
+using namespace std;
+
 int main()
 {
+  vector<int> A = {1, 2};
+  vector<int> B = {3, 4};
+
+  // lets generate a pairs sums AxB
+  vector<int> C;
+
+  for (vector<int>::const_iterator i = A.begin(); i != A.end(); ++i)
+  {
+    for (vector<int>::const_iterator j = B.begin(); j != B.end(); ++j)
+    {
+      C.push_back(*i + *j);
+    }
+  }
+
+  for (vector<int>::const_iterator psum = C.begin(); psum != C.end(); ++psum)
+  {
+    cout << *psum << endl;
+  }
+
   // important: clean up memory
   printf("\n");
   printf("Cleaning up memory...\n");
