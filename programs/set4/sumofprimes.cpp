320926f8de010d5636cd6d176d7dbec4d17939e6
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-11 15:46:48 +0530

    (feat): add sum-of-primes

---
+++ programs/set4/sumofprimes.cpp
@@ -0,0 +1,32 @@
+/** @summary
+ * Represent any number as the sum of primes (no unique) 
+*/
+
+#include <iostream>
+#include <vector>
+
+// used for exit function
+#include <math.h>
+#include <limits.h>
+
+using namespace std;
+
+/** Main Function
+ * @author Abhinav Robinson
+ * @returns void
+*/
+int main()
+{
+  // important: clean up memory
+  printf("\n");
+  printf("Cleaning up memory...\n");
+
+  //clear buffer, wait for input to close program
+  cout << "Enter Anything to Quit :)" << endl;
+  cin.clear();
+  cin.ignore(INT_MAX, '\n');
+  cin.get();
+
+  // End of Main
+  return 0;
+}
