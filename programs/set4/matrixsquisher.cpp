ff0f89c81e20879c56b4a91d27782172fbb51fa3
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-12 16:51:50 +0530

    (fix): add matrix-squisher

---
+++ programs/set4/matrixsquisher.cpp
@@ -0,0 +1,34 @@
+/** Given a 2D matrix, which is symmetric along it's diagonal;
+ * Compress the matrix into a 1D matrix of either upper Triangle, or lower Triangle (choose at runtime: 0,1)  
+ */
+
+#include <math.h>
+#include <limits.h>
+
+#include <vector>
+#include <iostream>
+
+// *** End Of Imports ***
+
+/**
+ * @function Main 
+ * @returns 0 { int }
+ * @author Abhinav Robinson
+*/
+int main()
+{
+  // important: clean up memory
+  printf("\n");
+  printf("Cleaning up memory...\n");
+
+  sides = 0;
+
+  //clear buffer, wait for input to close program
+  std::cout << "Enter Anything to Quit :)" << std::endl;
+  std::cin.clear();
+  std::cin.ignore(INT_MAX, '\n');
+  std::cin.get();
+
+  // End of Main
+  return 0;
+}
