99de337e4775723b73e60ede1848408201183998
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-17 13:30:42 +0530

    (feat): add sample matrix

--- programs/set5/squarespiral.cpp
+++ programs/set5/squarespiral.cpp
@@ -8,9 +8,40 @@ using namespace std;
 
 int main()
 {
+  cout << "Sample Matrix : " << endl;
+
+  // sample matrix
+  vector<vector<int>> matrix = {{1, 2, 3, 4},
+                                {5, 6, 7, 8},
+                                {9, 10, 11, 12},
+                                {13, 14, 15, 16}};
+
+  // print matrix
+  for (auto i : matrix)
+  {
+    cout << "[";
+    for (auto j : i)
+    {
+      cout << j << ",";
+    }
+    cout << "]\n";
+  }
+  cout << endl;
+
+  // lets convert to a spiral array
+  vector<int> spiral;
+
+  // for (int i = 0; i < matrix.size(); i++)
+  // {
+
+  // }
+
   // important: clean up memory
   printf("\n");
   printf("Cleaning up memory...\n");
+
+  matrix.clear();
+  spiral.clear();
 
   //clear buffer, wait for input to close program
   cout << "Enter Anything to Quit :)" << std::endl;
