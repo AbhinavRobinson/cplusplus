274b6da9df3f025e40042ddb43f6c5b516b9b605
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-17 14:43:24 +0530

    (feat): gen matrix and print

--- programs/set5/matrixencrypt.cpp
+++ programs/set5/matrixencrypt.cpp
@@ -4,6 +4,37 @@
 #include <math.h>
 #include <limits.h>
 
+std::vector<std::vector<int>> generateIdentityMatrix(int size)
+{
+  std::vector<std::vector<int>> matrix;
+
+  for (int i = 0; i <= size; i++)
+  {
+    matrix.push_back({});
+    for (int j = 0; j <= size; j++)
+    {
+      matrix[i].push_back(1);
+    }
+  }
+
+  return matrix;
+}
+
+void print2DMatrix(std::vector<std::vector<int>> matrix)
+{
+  for (std::vector<std::vector<int>>::const_iterator i = matrix.begin(); i != matrix.end(); ++i)
+  {
+    std::cout << "[";
+    for (std::vector<int>::const_iterator j = i->begin(); j != i->end(); ++j)
+    {
+      std::cout << *j << ",";
+    }
+    std::cout << "]\n";
+  }
+  std::cout << std::endl;
+  return;
+}
+
 /**
  * @brief Apply matrix operations to encrypt.
  * 
@@ -15,6 +46,12 @@
  */
 int main()
 {
+  std::cout << "Enter size of matrix (3+) : ";
+  int choice;
+  std::cin >> choice;
+
+  print2DMatrix(generateIdentityMatrix(choice));
+
   // important: clean up memory
   printf("\n");
   printf("Cleaning up memory...\n");
