a0684cee4790ea1b4f7e38c4797505a0b9cfdf79
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-18 10:29:51 +0530

    (feat): add overloaded compare

---
+++ programs/set5/overloadedcompare.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+#include <math.h>
+#include <limits.h>
+
+/**
+ * @brief Have a overloaded compare function
+ * 
+ * take args 0,1,2 -> int, char, string
+ * 
+ * take 2 args, N1, N2, values to be compared
+ * 
+ * if N1 > N2 => 1
+ * if N2 > N1 => -1
+ * else 0
+ * 
+ * @return int 
+ */
+int main()
+{
+  return 0;
+}
