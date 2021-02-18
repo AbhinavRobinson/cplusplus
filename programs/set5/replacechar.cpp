a2d7cb54a47e83a23cd6f4c2fa151abd7c5565b1
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-18 13:44:14 +0530

    (feat): add replace-char

---
+++ programs/set5/replacechar.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+#include <math.h>
+#include <limits.h>
+#include <stdexcept>
+
+int main()
+{
+  // important: clean up memory
+  printf("\n");
+  printf("Cleaning up memory...\n");
+
+  //clear buffer, wait for input to close program
+  std::cout << "Enter Anything to Quit :)" << std::endl;
+  std::cin.clear();
+  std::cin.ignore(INT_MAX, '\n');
+  std::cin.get();
+
+  return 0;
+}
