624b3633e7f3a0282a759901f2f804026876c336
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-18 14:02:33 +0530

    (feat): add guards

--- programs/set5/replacechar.cpp
+++ programs/set5/replacechar.cpp
@@ -1,16 +1,33 @@
-#include <vector>
 #include <iostream>
-#include <math.h>
 #include <limits.h>
+#include <math.h>
 #include <stdexcept>
+#include <vector>
 
 int main()
 {
+  // get input
+  std::string inputString;
+  std::cout << "Enter Imput String : ";
+  std::cin >> inputString;
+
+  char toReplace;
+  std::cout << "Enter Charachter to replace : ";
+  std::cin >> toReplace;
+
+  char replaceWith;
+  std::cout << "Enter Charachter to replace with : ";
+  std::cin >> replaceWith;
+
+  // guards
+  if (toReplace == ' ' || toReplace == NULL) return 0 && std::cout << inputString;
+  if (replaceWith == ' ' || replaceWith == NULL) return 0 && std::cout << inputString;
+
   // important: clean up memory
   printf("\n");
   printf("Cleaning up memory...\n");
 
-  //clear buffer, wait for input to close program
+  // clear buffer, wait for input to close program
   std::cout << "Enter Anything to Quit :)" << std::endl;
   std::cin.clear();
   std::cin.ignore(INT_MAX, '\n');
