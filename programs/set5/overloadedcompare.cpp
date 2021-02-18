c5062129582383c8548e0e3c867c9d84fcbf1b42
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-18 12:20:50 +0530

    (feat): use std::variant

--- programs/set5/overloadedcompare.cpp
+++ programs/set5/overloadedcompare.cpp
@@ -2,6 +2,35 @@
 #include <iostream>
 #include <math.h>
 #include <limits.h>
+#include <stdexcept>
+#include <tuple>
+#include <variant>
+
+using variable_type = std::variant<int, char, std::string>;
+
+std::tuple<variable_type, variable_type> itemGetter(int type)
+{
+  // int
+  if (type == 0)
+  {
+    int a, b;
+    return std::make_tuple(a, b);
+  }
+  // char
+  if (type == 1)
+  {
+    char a, b;
+    return std::make_tuple(a, b);
+  }
+  // string
+  if (type == 2)
+  {
+    std::string a, b;
+    return std::make_tuple(a, b);
+  }
+  // invalid type
+  throw std::invalid_argument("Invalid type!");
+}
 
 /**
  * @brief Have a overloaded compare function
@@ -18,5 +47,20 @@
  */
 int main()
 {
+  int type;
+
+  std::cout << "Enter type of variable (0,1,2) => (int, chr, str): ";
+  std::cin >> type;
+
+  auto [a, b] = itemGetter(type);
+
+  std::cout << "Enter variable A : ";
+  std::cin >> a;
+
+  std::cout << "Enter variable B : ";
+  std::cin >> b;
+
+  // std::cout << &a << &b << std::endl;
+
   return 0;
 }
