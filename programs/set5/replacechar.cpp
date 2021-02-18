69e93ce8f35fdee5ae86ea2fd0b0f7ffbcc70b3a
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-18 14:16:36 +0530

    (feat): cmp replaceChar

--- programs/set5/replacechar.cpp
+++ programs/set5/replacechar.cpp
@@ -8,24 +8,39 @@ int main()
 {
   // get input
   std::string inputString;
-  std::cout << "Enter Imput String : ";
+  std::cout << "Enter Input String (no spaces) : ";
   std::cin >> inputString;
 
+  // get to replace char
   char toReplace;
   std::cout << "Enter Charachter to replace : ";
   std::cin >> toReplace;
 
+  // get char to replace with
   char replaceWith;
   std::cout << "Enter Charachter to replace with : ";
   std::cin >> replaceWith;
 
-  // guards
-  if (toReplace == ' ' || toReplace == NULL) return 0 && std::cout << inputString;
-  if (replaceWith == ' ' || replaceWith == NULL) return 0 && std::cout << inputString;
+  // convert to vector
+  std::vector<char> stringVector;
+  std::copy(inputString.begin(), inputString.end(), std::back_inserter(stringVector));
+
+  // iterate on vector and replace
+  for (int i = 0; i < stringVector.size(); i++)
+    if (stringVector[i] == toReplace) stringVector[i] = replaceWith;
+
+  // print result
+  for (auto i : stringVector)
+    std::cout << i;
 
   // important: clean up memory
   printf("\n");
   printf("Cleaning up memory...\n");
+
+  stringVector.clear();
+  inputString = "";
+  toReplace = ' ';
+  replaceWith = ' ';
 
   // clear buffer, wait for input to close program
   std::cout << "Enter Anything to Quit :)" << std::endl;
