89f51d5be0c6998750c5c75fada11e00f1b66ca8
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-23 11:08:37 +0530

    (feat): get input

--- programs/set6/splitter.cpp
+++ programs/set6/splitter.cpp
@@ -3,8 +3,27 @@
 #include <math.h>
 #include <vector>
 
+/**
+ * @brief Break down a list to it's smallest components.
+ * 
+ * @return int 
+ */
 int main()
 {
+  std::vector<std::vector<std::string>> stringList = {{}};
+
+  std::cout << "Enter Strings (enter End or end to stop):" << std::endl;
+
+  while (true)
+  {
+    std::string temp;
+    std::cin >> temp;
+
+    if (temp == "End" || temp == "end") break;
+
+    stringList[0].push_back(temp);
+  }
+
   // important: clean up memory
   printf("\n");
   printf("Cleaning up memory...\n");
