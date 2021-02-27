544b1f29ebeaeee7490e2fc136d41f378f2d8ea4
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-27 15:47:59 +0530

    (feat): add min swaps

--- programs/set6/minswaps.cpp
+++ programs/set6/minswaps.cpp
@@ -2,6 +2,7 @@
 #include <limits.h>
 #include <math.h>
 #include <vector>
+#include <bits/stdc++.h>
 
 /**
  * @brief Min swaps required to sort an array
@@ -25,6 +26,41 @@ int main()
     std::cin >> temp;
     unsortedArray.push_back(temp);
   }
+
+  std::pair<int, int> arrayPositionPair[size];
+  for (int i = 0; i < size; i++)
+  {
+    arrayPositionPair[i].first = unsortedArray[i];
+    arrayPositionPair[i].second = i;
+  }
+
+  std::sort(arrayPositionPair, arrayPositionPair + size);
+
+  std::vector<bool> visited(size, false);
+
+  int swaps = 0;
+
+  for (int i = 0; i < size; i++)
+  {
+    if (visited[i] || arrayPositionPair[i].second == i)
+      continue;
+
+    int cycle_size = 0;
+    int j = i;
+    while (!visited[j])
+    {
+      visited[j] = 1;
+      j = arrayPositionPair[j].second;
+      cycle_size++;
+    }
+    if (cycle_size > 0)
+    {
+      swaps += cycle_size - 1;
+    }
+  }
+
+  std::cout << "Swaps Required : ";
+  std::cout << swaps << std::endl;
 
   // important: clean up memory
   printf("\n");
