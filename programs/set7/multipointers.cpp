692c0153f4aa1305b904df7cb5352e96c304553d
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-03-06 20:44:02 +0530

    (feat): add guards

--- programs/set7/multipointers.cpp
+++ programs/set7/multipointers.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
+#include <stdexcept>
 #include <vector>
 
 int main()
 {
   // ---------------------------------------------
   // get arrays
-  std::vector<int> *wArr;
   int input;
+  std::vector<int> *wArr;
   std::cout << "Enter W Array (enter -1 to end) : ";
   while ((std::cin >> input) && input != -1)
     wArr->push_back(input);
+  // sanity check
+  if (wArr->size() < 1) throw std::invalid_argument("No items in W array");
   std::vector<int> *mArr;
   std::cout << "Enter M Array (enter -1 to end) : ";
   while ((std::cin >> input) && input != -1)
     mArr->push_back(input);
+  // sanity check
+  if (mArr->size() < 1) throw std::invalid_argument("No items in M array");
   std::vector<int> *sArr;
   std::cout << "Enter S Array (enter -1 to end) : ";
   while ((std::cin >> input) && input != -1)
     sArr->push_back(input);
+  // sanity check
+  if (sArr->size() < 1) throw std::invalid_argument("No items in S array");
   // done taking arrays
   // ---------------------------------------------
-
-  
+  // get score (main logic)
+  int score = 0;
+  if (mArr->size() != wArr->size()) throw std::invalid_argument("M and W arrays of unequal lengths");
   return 0;
 }
