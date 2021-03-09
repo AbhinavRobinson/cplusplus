46dee052ecc19970a2ffa77b2e17d4d7d8bc2db0
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-03-09 16:23:50 +0530

    (feat): add collector object

--- programs/set8/structs.cpp
+++ programs/set8/structs.cpp
@@ -10,12 +10,41 @@ struct Date
 
 struct Rainfall_Data
 {
-  std::vector<struct Date> Date;
+  struct Date Date;
   int rainfall;
 };
 
 void get_rainfall_data(std::vector<struct Rainfall_Data> *rainfall_data_array)
 {
+  std::cout << "Enter Rainfall Data -" << std::endl;
+  struct Rainfall_Data rainfall_data_collector;
+
+  int day, month, year, rainfall_amount;
+  try
+  {
+    std::cout << "Enter Day of Day?";
+    std::cin >> day;
+    std::cout << "Enter Day of Month?";
+    std::cin >> month;
+    std::cout << "Enter Day of Year?";
+    std::cin >> year;
+    std::cout << "Enter Day of Rainfall Amount?";
+    std::cin >> rainfall_amount;
+  }
+  catch (const std::exception &err)
+  {
+    std::cout << "Failed : Invalid data provided! Try again ...";
+    return;
+  }
+
+  rainfall_data_collector.rainfall = rainfall_amount;
+  rainfall_data_collector.Date.day = day;
+  rainfall_data_collector.Date.month = month;
+  rainfall_data_collector.Date.year = year;
+
+  std::cout << "Success : Information added to object!" << std::endl;
+
+  rainfall_data_array->push_back(rainfall_data_collector);
   return;
 }
 
@@ -35,8 +64,8 @@ int main()
   {
     if (input == 1) get_rainfall_data(&rainfall_data_array);
     if (input == 2) print_rainfall_data(&rainfall_data_array);
-    if (input != 1 && input != 2) std::cout << "Incorrect Option\n";
+    if (input != 1 && input != 2) std::cout << "Failed : Incorrect option\n";
   }
-  std::cout << "Exiting Program" << std::endl;
+  std::cout << "Exiting Program ..." << std::endl;
   return 0;
 }
