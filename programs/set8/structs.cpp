cfc802d174a19577026b31eda2a512f89497c773
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-03-09 16:07:54 +0530

    (feat): implement array of structs

--- programs/set8/structs.cpp
+++ programs/set8/structs.cpp
@@ -1,32 +1,40 @@
 #include <iostream>
+#include <vector>
+
+struct Date
+{
+  int day;
+  int month;
+  int year;
+};
 
 struct Rainfall_Data
 {
-  int day_of_month;
-  int month;
-  int year;
+  std::vector<struct Date> Date;
   int rainfall;
 };
 
-void get_rainfall_data()
+void get_rainfall_data(std::vector<struct Rainfall_Data> *rainfall_data_array)
 {
   return;
 }
 
-void print_rainfall_data()
+void print_rainfall_data(std::vector<struct Rainfall_Data> *rainfall_data_array)
 {
   return;
 }
 
 int main()
 {
+  std::vector<struct Rainfall_Data> rainfall_data_array; // declare array of struct Rainfall_Data
+
   std::cout << "Create new entry in Rainfall Object\nEnter (1) to add Entry\nEnter (2) to Print Data\nEnter (3) to Exit" << std::endl;
   std::cout << "Enter-->";
   int input;
   while ((std::cin >> input) && input != 3)
   {
-    if (input == 1) get_rainfall_data();
-    if (input == 2) print_rainfall_data();
+    if (input == 1) get_rainfall_data(&rainfall_data_array);
+    if (input == 2) print_rainfall_data(&rainfall_data_array);
     if (input != 1 && input != 2) std::cout << "Incorrect Option\n";
   }
   std::cout << "Exiting Program" << std::endl;
