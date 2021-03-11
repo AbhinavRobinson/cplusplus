52978b567c97633550c5b7e5017a85efebcb7c0e
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-03-11 10:37:32 +0530

    (feat): define class

--- programs/set8/justclass.cpp
+++ programs/set8/justclass.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <stdexcept>
 
+class Employee
+{
+  int employee_id;
+  std::string employee_name;
+  std::string employee_designation;
+  double monthly_salary;
+
+  public:
+  void add_employee_data(int id, std::string name, std::string desig, double sal);
+  void print_employee_data();
+};
+
 int main()
 {
   return 0;
