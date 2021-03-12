f4ab0e35543b4d584a998ad89f52ffedf8dc3d96
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-03-12 15:51:19 +0530

    (feat): add students

---
+++ programs/set8/students.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+class Student
+{
+  public:
+  int roll_no;
+  std::string name;
+};
+
+class ClassRoom
+{
+  Student students;
+};
+
+int main()
+{
+
+  return 0;
+}
